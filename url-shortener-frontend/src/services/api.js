7e4344901b22dbce1aa6f4f3cfc58ec6f404dd75
Midhun Mathew <midhunmathew169@gmail.com>
2026-01-06 11:21:29 +0530

    checking api url

--- url-shortener-frontend/src/services/api.js
+++ url-shortener-frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_BASE_URL
+const API_URL = import.meta.env.VITE_API_BASE_URL;
+
+console.log("VITE_API_BASE_URL:",API_URL);
 
 const api = axios.create({
     baseURL: API_URL,
